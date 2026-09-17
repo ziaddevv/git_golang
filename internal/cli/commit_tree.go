61ef6390f2343234d1610e48c07f3f2e2d311ed6
ziad <zyyyad1990@gmail.com>
2026-09-17 14:40:57 +0300

    udpate commit-tree error handling

--- internal/cli/commit_tree.go
+++ internal/cli/commit_tree.go
@@ -8,7 +8,7 @@ import (
 	flag "github.com/spf13/pflag"
 )
 
-func CommitTreeCommand() {
+func CommitTreeCommand() error {
 	// mygit commit-tree <tree-hash> -m "message"
 	// mygit commit-tree <tree-hash> -p <parent-commit> -m "message"
 	cmd := flag.NewFlagSet("commit-tree", flag.ExitOnError)
@@ -20,34 +20,24 @@ func CommitTreeCommand() {
 	cmd.Parse(os.Args[2:])
 
 	if cmd.NArg() < 1 {
-		fmt.Fprintln(os.Stderr, "usage: mygit commit-tree <tree-hash> -m <message> [-p <parent>]...")
-		os.Exit(1)
+		return usageError("usage: mygit commit-tree <tree-hash> -m <message> [-p <parent>]...")
 	}
 
 	treeHash := cmd.Arg(0)
 
 	if !object.IsValidObjectID(treeHash) {
-		fmt.Fprintf(os.Stderr, "fatal: not a valid object name: %s\n", treeHash)
-		os.Exit(1)
+		return fmt.Errorf("not a valid object name: %s", treeHash)
 	}
 
 	if *message == "" {
-		fmt.Fprintln(os.Stderr, "fatal: commit message is required (-m)")
-		os.Exit(1)
+		return fmt.Errorf("commit message is required (-m)")
 	}
 
 	for _, p := range *parentHashes {
 		if !object.IsValidObjectID(p) {
-			fmt.Fprintf(os.Stderr, "fatal: not a valid parent hash: %s\n", p)
-			os.Exit(1)
+			return fmt.Errorf("not a valid parent hash: %s", p)
 		}
 	}
 
-	err := object.CommitObject(treeHash, *message, *parentHashes)
-
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "fatal:", err)
-		os.Exit(1)
-	}
+	return object.CommitObject(treeHash, *message, *parentHashes)
 }
-
